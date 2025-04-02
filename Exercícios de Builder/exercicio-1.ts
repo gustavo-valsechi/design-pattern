f5f27659521af867f8d628e484b30df1d2e41b95
Gustavo Valsechi De Freitas <gustavo.24110@alunosatc.edu.br>
2025-04-02 20:33:30 -0300

    Builders

---
+++ Exercícios de Builder/exercicio-1.ts
@@ -0,0 +1,42 @@
+interface IBurgerOrderBuilder {
+    setTipoPao(tipo: number): void;
+    setProteina(tipo: number): void;
+}
+
+class BurgerOrderBuilder implements IBurgerOrderBuilder {
+    constructor() {
+
+    }
+
+    setTipoPao() {
+
+    }
+
+    setProteina() {
+        
+    }
+
+    setQueijo() {
+        
+    }
+
+    setAlface() {
+        
+    }
+
+    setTomate() {
+        
+    }
+
+    setCebola() {
+        
+    }
+
+    setMolhoEspecial() {
+        
+    }
+
+    setBebida() {
+        
+    }
+}
